97905372fb3ede8dc7812066cd9e7f212d12b3f3
DavidRomy <934803813@qq.com>
2024-01-29 03:43:06 +0800

    update script

--- scripts/2_run_test.js
+++ scripts/2_run_test.js
@@ -9,69 +9,34 @@ const { expect } = require("chai");
 const configFile = process.cwd() + "/scripts/config.json";
 const jsonfile = require('jsonfile');
 
-const PreReady = 0;  // 0: 预准备状态
-const Ready = 1;     // 1：准备状态
-const Active = 2;    // 2: 游戏中
-const Over = 3;       // 3:游戏结束
-
-let TableId = 0; // 记录tableId
-let game;
-
-// 参与游戏
-async function joinTable(TableId, player) {
-  // 参与游戏
-  await game.connect(player).joinTable(TableId);
-}
-
-// 查看玩家信息
-async function getPlayerInfo(TableId, playerAddr) {
-  console.log("getPlayerInfo=======");
-  info = await game.getPlayerInfo(TableId, playerAddr);
-  console.log("       player:",playerAddr, ", isInTable:", info.isInTable, ", cards:", info.cards);
-}
-
-// 查看游戏桌信息
-async function getTableInfo(TableId) {
-  console.log("getTableInfo, tableID:", TableId);
-  table = await game.getTableInfo(TableId);
-  console.log("       state:",table.state, ", round:", table.round, ", battleCards:", table.battleCards, ", playerAddrList:", table.playerAddrList);
-  return table
-}
-
 async function main() {
   let config = await jsonfile.readFileSync(configFile);
 
-  const [deployer, player1, player2] = await ethers.getSigners();
+  const [deployer, addr1, addr2] = await ethers.getSigners();
+  console.log("addr1:", addr1.address, ",addr2:", addr2.address);
 
-  // game合约
-  const RockPaperScissorsGame = await ethers.getContractFactory("RockPaperScissorsGame");
-  game = RockPaperScissorsGame.attach(config.ethSeries.RockPaperScissorsGame);
+  // 铭文合约
+  const TurnScription = await ethers.getContractFactory("TurnScription");
+  inscription = TurnScription.attach(config.ethSeries.TurnScription);
   
-  console.log("Start to create game table");
-  tx = await game.createTable();
-  console.log("Create game table successful:", tx);
+  // 批量铸币
+  const times = 2;
+  for(let i = 0; i < times; i++) {
+    console.log("Start to mint TurnScription ==> i:", i);
+    const randNum = "afadf!@#$%^1231ASDF";  // 随机数
+    tx = await inscription.connect(addr1).mint(randNum);
+    console.log("Mint TurnScription successful, i:", i, ", tx:", tx);
+  }
 
-  console.log("Player start to join game table, tableID:", TableId);
-  await joinTable(TableId, player1);
-  await joinTable(TableId, player2);
-  console.log("Player join game table successful");
+  // 获取addr1的铭文信息
+  add1PaperInfos = await inscription.getUserPaperInfos(addr1.address);
+  console.log("addr1 paper infos:", add1PaperInfos.pidList_);
+  expect(add1PaperInfos.pidList_.length).to.equal(times);  // 玩家人数
 
-  console.log("player2 readyGame");
-  await game.connect(player2).readyGame(TableId);
-
-  console.log("player1 startGame");
-  await game.connect(player1).startGame(TableId);
-
-  // 获取玩家将的卡牌
-  await getPlayerInfo(TableId, player1.address);
-  await getPlayerInfo(TableId, player2.address);
-
-
-  tableInfo = await getTableInfo(TableId);
-  // 游戏状态
-  expect(tableInfo.state).to.equal(Active);
-  expect(tableInfo.round).to.equal(0);  
-  expect(tableInfo.playerAddrList.length).to.equal(2);  // 玩家人数
+  // 批量转账
+  console.log("Start to batch transfer TurnScription");
+  tx = await inscription.connect(addr1).batchTransfer(addr2.address, [1,2]);
+  console.log("Mint TurnScription successful, tx:", tx);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
